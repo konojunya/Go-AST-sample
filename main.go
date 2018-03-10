21f61ab6204ddc14fa3a81e2ab387b39f8188072
konojunya <junya03dance@gmail.com>
2018-03-10 14:58:55 +0900

    hello、世界を自動生成

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"go/ast"
+	"go/format"
+	"go/token"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f := &ast.File{
+		Name: ast.NewIdent("main"),
+		Decls: []ast.Decl{
+			&ast.GenDecl{
+				Tok: token.IMPORT,
+				Specs: []ast.Spec{
+					&ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: strconv.Quote("fmt"),
+						},
+					},
+				},
+			},
+			&ast.FuncDecl{
+				Name: ast.NewIdent("main"),
+				Type: &ast.FuncType{},
+				Body: &ast.BlockStmt{
+					List: []ast.Stmt{
+						&ast.ExprStmt{
+							X: &ast.CallExpr{
+								Fun: &ast.SelectorExpr{
+									X:   ast.NewIdent("fmt"),
+									Sel: ast.NewIdent("Println"),
+								},
+								Args: []ast.Expr{
+									&ast.BasicLit{
+										Kind:  token.STRING,
+										Value: strconv.Quote("Hello, 世界"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	format.Node(os.Stdout, token.NewFileSet(), f)
+}
